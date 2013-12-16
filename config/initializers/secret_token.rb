c373026174c2cf97bf9af19fe19166ead8a2891b
veluria <vlack.lavel@gmail.com>
2013-12-15 22:46:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '38b9fcc1068698cb61c28611356b5fa8bf98c87bb3e0461785ea7a9ea28c845904285659ae696462a0adbdcb10ac6c2b8d300c96dd8379aaac2679876f13fb21'
